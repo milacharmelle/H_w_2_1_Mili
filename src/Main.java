95e416bf1c731a49b9491d7b9b202c7985c363c3
Your Name <102684272+milacharmelle@users.noreply.github.com>
2022-07-18 21:34:38 +0500

    git commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketPrice = 13_205;
+        int mili = 20;
+        int getMili = ticketPrice / mili;
+
+        System.out.println(getMili);
+    }
+}
